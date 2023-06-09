9441724e3cca378cd8791f43b6ed250949d5461b
vishnumohanrk <mohanvish1999@gmail.com>
2023-06-09 09:19:46 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function AppHome() {
+  return (
+    <p className="text-neutral-400">
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, non
+      vitae, consequuntur fuga ullam voluptates ex deleniti, incidunt aperiam
+      assumenda tempore ipsa placeat dolore? Porro in et veniam maiores quidem?
+    </p>
+  );
+}
